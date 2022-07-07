38e88f4d516fdcc572cf20f4eafc085f376d6d12
Daniel Schnöll <daniel.schnoell@tuwien.ac.at>
2022-07-07 16:09:08 +0200

    to test

--- model/convolution.py
+++ model/convolution.py
@@ -77,20 +77,74 @@ class Conv2dQuant(nn.Conv2d):
             print("post convolution not whole number",(out-out.round()).mean())
         return out, orexp
 
-    
 
-class LinQuantWeight_new(torch.autograd.Function):
+class LinQuant_new_(torch.autograd.Function):
     @staticmethod
-    def forward(_,x,factor_fun,rexp_diff,quant):
+    def forward(self, x, abs, delta,rexp_diff,fact):
         with torch.no_grad():
-            x = x*(2**rexp_diff)[None, :, None, None]
-            x, fact = quant(x, factor_fun)
-            val_float = x/(2**rexp_diff)[None, :, None, None]
-            val_int = x*fact
-        return val_float, val_int
+            x = x*(2**rexp_diff)[None, :, None, None]*fact
+            self.save_for_backward(x, abs)
+            x = x.clamp(-abs, abs)
+            x = x.div(delta, rounding_mode="floor").mul(delta)
+            x = x/((2**rexp_diff)[None, :, None, None]*fact)
+            if torch.any(torch.isnan(x)):
+                print("nan in Linquant forward")
+            return x
+        
     @staticmethod
-    def backward(_, grad_float, grad_int):
-        return grad_float, None, None, None
+    def backward(self, grad_output: torch.Tensor):
+        with torch.no_grad():
+            val = 1
+            x, abs = self.saved_tensors
+            grad_output = grad_output.masked_fill(torch.logical_and(
+                torch.gt(x, val*abs), torch.gt(grad_output, 0)), 0)
+            grad_output = grad_output.masked_fill(torch.logical_and(
+                torch.le(x, -val*abs), torch.le(grad_output, 0)), 0)
+            if torch.any(torch.isnan(grad_output)):
+                print("nan in Linquant back")
+            return grad_output.detach(), None, None, None, None
+
+
+class LinQuantWeight(Quant):
+    def __init__(self, bits, size=(-1,), mom1=0.1, mom2=0.01) -> None:
+        super(LinQuantWeight, self).__init__(size)
+        self.bits = bits
+        if size == (-1,):
+            self.register_buffer('abs', torch.ones(1))
+        else:
+            self.register_buffer('abs', torch.ones(size))
+        self.take_new = True
+        self.mom1 = mom1
+        self.mom2 = mom2
+
+    def forward(self, x:torch.Tensor,rexp_diff, fact_fun=None):
+        with torch.no_grad():
+            abs = get_abs(self,x*(2**rexp_diff)[None, :, None, None])
+            if torch.any(abs < 1e-6):
+                print("weights to small to quantize")
+                self.delta = (2*(self.abs/(2.0**self.bits-1.0))).detach()
+                return LinQuant_.apply(x*fact, self.abs, self.delta)
+
+            if self.training and self.take_new:
+                self.abs = abs.detach()
+                self.take_new = False
+                # print("new taken")
+            elif self.training:
+                # print(f" abs diff:  {(abs.view(-1)-self.abs.view(-1)).abs().max()}")
+                self.abs = ((1-self.mom1-self.mom2)*self.abs + self.mom1*abs + self.mom2 *
+                            (self.abs/(2.0**self.bits-1.0)) * (2.0**self.bits-1.0)).detach()
+            # print(f" old delta: {self.delta.view(-1)}")
+            self.delta = (2*(self.abs/(2.0**self.bits-1.0))).detach()
+            # print(f" new delta: {self.delta.view(-1)}")
+            if fact_fun!=None:
+                fact = fact_fun(self.delta)
+            else:
+                fact = 1
+            if torch.any(torch.isnan(self.delta)):
+                print("nan in weights")
+        # print((self.delta).shape)
+        return LinQuant_new_.apply(x, self.abs, self.delta,rexp_diff,fact),fact
+
 
 class Conv2dQuant_new(nn.Conv2d):
     def __init__(self,  in_channels: int, out_channels: int, kernel_size: _size_2_t, stride: _size_2_t = 1,
@@ -121,20 +175,20 @@ class Conv2dQuant_new(nn.Conv2d):
     def forward(self, invals: Tuple[torch.Tensor, torch.Tensor], factor_fun=None) -> torch.Tensor:
         input, rexp = invals
 
-        tmp = self.weight.clone()
-
         orexp = (torch.mean(rexp)).squeeze()
         rexp_diff = rexp.squeeze() - orexp.unsqueeze(-1)
 
-        tmp_float,fact_int = LinQuantWeight_new.apply(self.weight,factor_fun,rexp_diff,self.quantw.forward)
+        tmp = self.weight
+
+        tmp,fact = self.quantw(tmp,rexp_diff,factor_fun)
+        # tmp = self.weight
         
         if not self.training:
-            tmp = torch.round(fact_int/self.quantw.delta)
+            # fact = 1
+            tmp = torch.round(tmp*fact*(2**rexp_diff)[None, :, None, None]/self.quantw.delta)
             tmp = checkNan.apply( tmp, "conv tmp 2")
             # only nessesary as /delta can have a slight relative error ~1e-6 in calculations
             self.quant_weight = tmp.detach()
-        else:
-            tmp = tmp_float
 
         if torch.any(torch.isnan(tmp)):
             print(torch.max(torch.abs(self.weight.view(-1))))
@@ -162,8 +216,7 @@ class Conv2dQuantFreeStanding_new(Conv2dQuant_new):
         if self.bias!=None:
             self.register_buffer('quant_bias', torch.zeros_like(self.bias))
     def forward(self, invals: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
-        val = checkNan.apply(invals[0], "Conv fs in val")
-        x = super().forward((val,invals[1]),None)
+        x = super().forward(invals,None)
         rexp = x[1]+torch.log2(self.quantw.delta)
         val = x[0]
         val = checkNan.apply(val,"Conv fs val 1")
